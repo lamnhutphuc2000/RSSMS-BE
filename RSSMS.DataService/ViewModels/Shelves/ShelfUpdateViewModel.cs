084c3aabd597f86ed143e4d54d62ec27281f3928
lamnhutphuc2000 <lamnhutphuc2000@gmail.com>
2021-10-14 16:04:25 +0700

    add shelf update view model

--- RSSMS.DataService/ViewModels/Shelves/ShelfUpdateViewModel.cs
+++ RSSMS.DataService/ViewModels/Shelves/ShelfUpdateViewModel.cs
@@ -6,7 +6,8 @@
         public int? Type { get; set; }
         public string Name { get; set; }
         public string Note { get; set; }
-        public int? BoxesInWidth { get; set; }
-        public int? BoxesInHeight { get; set; }
+        public int BoxesInWidth { get; set; }
+        public int BoxesInHeight { get; set; }
+        public int BoxSize { get; set; }
     }
 }
