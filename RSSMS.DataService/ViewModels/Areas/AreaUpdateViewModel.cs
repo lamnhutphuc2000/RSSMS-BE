e319594c9a944247e6dfcc03dbd647e57dd60e44
lamnhutphuc2000 <lamnhutphuc2000@gmail.com>
2021-10-25 13:53:21 +0700

    Add descriptin when update area

--- RSSMS.DataService/ViewModels/Areas/AreaUpdateViewModel.cs
+++ RSSMS.DataService/ViewModels/Areas/AreaUpdateViewModel.cs
@@ -6,5 +6,6 @@ namespace RSSMS.DataService.ViewModels.Areas
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
