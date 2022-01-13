458789eed7936153e8ef51a8ce4abbc1299910f1
lamnhutphuc2000 <lamnhutphuc2000@gmail.com>
2022-01-13 14:12:14 +0700

    Add order detail Id

--- RSSMS.DataService/ViewModels/OrderDetails/OrderDetailsViewModel.cs
+++ RSSMS.DataService/ViewModels/OrderDetails/OrderDetailsViewModel.cs
@@ -5,6 +5,7 @@ namespace RSSMS.DataService.ViewModels.OrderDetails
 {
     public class OrderDetailsViewModel
     {
+        public int Id { get; set; }
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal? Price { get; set; }
