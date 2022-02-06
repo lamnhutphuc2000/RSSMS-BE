f05bcf22d7b3e7f8597a01a336947a256474bd70
lamnhutphuc2000 <lamnhutphuc2000@gmail.com>
2022-02-06 12:36:10 +0700

    Fix notification can not get notification detail

--- RSSMS.DataService/Services/NotificationService.cs
+++ RSSMS.DataService/Services/NotificationService.cs
@@ -34,8 +34,8 @@ namespace RSSMS.DataService.Services
         public async Task<DynamicModelResponse<NotificationViewModel>> GetAll(int userId, string[] fields, int page, int size)
         {
             var notifications = Get(x => x.IsActive == true && x.NotificationDetails.Any(a => a.UserId == userId && a.IsActive == true))
-                .OrderByDescending(x => x.CreateDate)
-                .Include(a => a.NotificationDetails.Where(s => s.UserId == userId && s.IsActive == true)).ProjectTo<NotificationViewModel>(_mapper.ConfigurationProvider)
+                .Include(a => a.NotificationDetails.Where(r => r.UserId == userId)).ToList().AsQueryable()
+                .OrderByDescending(x => x.CreateDate).ProjectTo<NotificationViewModel>(_mapper.ConfigurationProvider)
                 .PagingIQueryable(page, size, CommonConstant.LimitPaging, CommonConstant.DefaultPaging);
 
             if (notifications.Item2.ToList().Count < 1) throw new ErrorResponse((int)HttpStatusCode.NotFound, "Can not found");
--- RSSMS.DataService/ViewModels/Notifications/NotificationViewModel.cs
+++ RSSMS.DataService/ViewModels/Notifications/NotificationViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RSSMS.DataService.Models;
+using System;
+using System.Collections.Generic;
 
 namespace RSSMS.DataService.ViewModels.Notifications
 {
