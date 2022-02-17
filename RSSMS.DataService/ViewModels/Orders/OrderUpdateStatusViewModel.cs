3d0a9bcea3c6dd513d42a75d3d9196b12d33a3b2
lamnhutphuc2000 <lamnhutphuc2000@gmail.com>
2022-02-17 14:30:56 +0700

    Add order update view model

---
+++ RSSMS.DataService/ViewModels/Orders/OrderUpdateStatusViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSSMS.DataService.ViewModels.Orders
+{
+    public class OrderUpdateStatusViewModel
+    {
+        public int Id { get; set; }
+        public int Status { get; set; }
+    }
+}
