38d623bafb7842fe3356369c81dfe8b6cffc5329
lamnhutphuc2000 <lamnhutphuc2000@gmail.com>
2022-04-01 17:20:13 +0700

    Change notification viewmodel

--- RSSMS.DataService/ViewModels/Notifications/NotificationViewModel.cs
+++ RSSMS.DataService/ViewModels/Notifications/NotificationViewModel.cs
@@ -4,13 +4,13 @@ namespace RSSMS.DataService.ViewModels.Notifications
 {
     public class NotificationViewModel
     {
-        public int Id { get; set; }
-        public int OrderId { get; set; }
-        public int RequestId { get; set; }
+        public Guid Id { get; set; }
+        public Guid? RequestId { get; set; }
         public string Description { get; set; }
-        public int Type { get; set; }
-        public bool IsOwn { get; set; }
         public bool IsRead { get; set; }
         public DateTime CreateDate { get; set; }
+        public Guid ReceiverId { get; set; }
+        public string Note { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
