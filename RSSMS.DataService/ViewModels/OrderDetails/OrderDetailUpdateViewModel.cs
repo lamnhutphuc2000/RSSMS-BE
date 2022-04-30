b0c84f9c2e98a677e50c56e66ccf7b8134a4bae9
lamnhutphuc2000 <lamnhutphuc2000@gmail.com>
2022-04-30 15:10:00 +0700

    Refactor code

---
+++ RSSMS.DataService/ViewModels/OrderDetails/OrderDetailUpdateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSSMS.DataService.ViewModels.OrderDetails
+{
+    public class OrderDetailUpdateViewModel
+    {
+        public Guid Id { get; set; }
+        public int Status { get; set; }
+        
+    }
+}
